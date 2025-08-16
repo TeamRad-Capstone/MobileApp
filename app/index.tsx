04f36ac317c9344ec14affcad9c6d7f40ba9d632
Chioma <96903876+chiomaeme@users.noreply.github.com>
2025-08-16 01:07:26 -0600

    Index and layout associated developed to match UI for just landing page background prior to adding the forms

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,35 @@
-import { Text, View } from "react-native";
+import {ImageBackground, StyleSheet, Text, View} from "react-native";
+import LandingScreen from "@/components/LandingScreen";
 
 export default function Index() {
-  return (
-    <View
-      style={{
+    return (
+        <View style={styles.container}>
+            <ImageBackground
+                source={require('@/assets/images/landing.jpg')}
+                style={styles.bgImage}
+                resizeMode="cover"
+            >
+                <LandingScreen>
+                </LandingScreen>
+            </ImageBackground>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
-}
+    },
+    bgImage: {
+        width: "100%",
+        height: "100%",
+        outlineOffset: 5,
+    },
+    text: {
+        fontSize: 18,
+        justifyContent: "center",
+        alignItems: "center",
+    }
+})
