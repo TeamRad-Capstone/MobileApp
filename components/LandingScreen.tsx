b4d8b32384678aead81eff19f23bca027812029e
Chioma <96903876+chiomaeme@users.noreply.github.com>
2025-08-16 00:52:15 -0600

    Landing pages parent component created

---
+++ components/LandingScreen.tsx
@@ -0,0 +1,37 @@
+import { StyleSheet, View, Text } from 'react-native';
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const LandingScreen = ({ children }: any) => {
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.welcomeContainer}>
+                <Text style={styles.welcomeText}>Welcome to Rad Reads</Text>
+            </View>
+            {children}
+        </SafeAreaView>
+    )
+}
+
+export default LandingScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: -40,
+        height: '82%',
+        borderStyle: 'solid',
+        borderTopWidth: 0,
+        borderWidth: 6,
+        borderRadius: 70,
+        borderColor: '#BE6A53',
+        backgroundColor: '#FDDCB9',
+    },
+    welcomeContainer: {
+        alignItems: 'center',
+        paddingTop: 30,
+    },
+    welcomeText: {
+        fontFamily: 'MPlus-Rounded-Black',
+        fontSize: 30,
+        paddingTop: 10,
+    }
+})
