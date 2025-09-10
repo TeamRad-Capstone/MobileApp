0fb2632d07d7d29c2843a03944af31036c8190e7
Chioma <96903876+chiomaeme@users.noreply.github.com>
2025-09-09 18:57:40 -0600

    Update search.tsx
    Search page updated to search in Google Books API for retrieving books and displaying them

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,9 +1,60 @@
 import {View, StyleSheet, Text, Image, ScrollView, TextInput} from "react-native";
 import {useState} from "react";
 import {SafeAreaView} from "react-native-safe-area-context";
+import SearchBook from "@/components/SearchBook";
 
+type SearchBookType = {
+    coverUrl: string;
+    title: string;
+    authors: string[];
+    description: string;
+    numOfPages: number;
+    mainCategory: string;
+    categories: string[];
+    publishedDate: string;
+}
 const Search = () => {
     const [searchParam, setSearchParams] = useState("");
+    const [returnedBooks, setReturnedBooks] = useState<SearchBookType[]>([]);
+    // const [queryType, setQueryType] = useState("title");
+
+    const searchInAPI = async () => {
+        setReturnedBooks([])
+
+        let volumeQueryUrl = "https://www.googleapis.com/books/v1/volumes?q=";
+        let maxResults = "&maxResults=20";
+        let type = "title";
+        let queryType = `&in${type}= ` + searchParam;
+
+        try {
+            const response = await fetch(volumeQueryUrl + searchParam + queryType+ maxResults +
+                "&key=" + process.env.EXPO_PUBLIC_GOOGLE_BOOKS_API_KEY
+            );
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            console.log("Query Search: " + searchParam);
+            for (const books of data.items) {
+                let newBook = {
+                    coverUrl: `https://books.google.com/books?id=${books.id}&printsec=frontcover&img=1&zoom=4&edge=curl&source=gbs_api`,
+                    title: books.volumeInfo.title,
+                    authors: books.volumeInfo.authors,
+                    description: books.description,
+                    numOfPages: books.numOfPages,
+                    mainCategory: books.mainCategory,
+                    categories: books.categories,
+                    publishedDate: books.publishedDate,
+                }
+                console.log(newBook);
+                //numOfBooks++;
+                setReturnedBooks(oldBooks => [...oldBooks, newBook])
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     return (
         <SafeAreaView style={styles.container}>
@@ -17,12 +68,26 @@ const Search = () => {
                     style={styles.searchInput}
                     value={searchParam}
                     onChangeText={setSearchParams}
+                    onSubmitEditing={searchInAPI}
                 />
             </View>
             <ScrollView>
             {/* Add a way to load books from searching through API
                 Send search params to backend API and do retrieval from OpenBooks API
                 for data to parse in the app. */}
+                {returnedBooks.map((book, index) => (
+                    <SearchBook
+                        key={index}
+                        coverUrl={book.coverUrl}
+                        title={book.title}
+                        authors={book.authors}
+                        description={book.description}
+                        numOfPages={book.numOfPages}
+                        mainCategory={book.mainCategory}
+                        categories={book.categories}
+                        publishedDate={book.publishedDate}
+                    />
+                ))}
             </ScrollView>
         </SafeAreaView>
     )
